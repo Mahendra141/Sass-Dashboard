ed6af4dfce6afb120bffd1f04aa4d8057736cd99
mahendra <yarrammahendra76@gmail.com>
2024-11-07 00:26:18 +0530

    intial commit

---
+++ webpack.config.cjs
@@ -0,0 +1,41 @@
+// webpack.config.cjs
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
+    historyApiFallback: true, 
+  },
+  resolve: {
+    extensions: ['.js', '.jsx','cjx'],
+  },
+};
